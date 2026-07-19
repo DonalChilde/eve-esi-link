b7dee6b0b7bb28964277c98de3e813c7aa587765
Chad Lowe <pfmsoft.dev@gmail.com>
2026-07-19 15:56:06 -0700

    update application name and add PyPI publish workflow
    * Change application name to 'pfmsoft-eve-link' in __init__.py
    * Add GitHub Actions workflow for publishing to PyPI
    * Update environment variable in .env.dev

--- src/pfmsoft/eve_link/__init__.py
+++ src/pfmsoft/eve_link/__init__.py
@@ -8,19 +8,16 @@ For library usage, EsiLink is the primary entrypoint. See:
 - docs/schema-package-contracts.md
 """
 
+from importlib.metadata import version
+
 __project_namespace__ = "pfmsoft"
 __author__ = "Chad Lowe"
 __email__ = "pfmsoft.dev@gmail.com"
-__app_name__ = "eve-esi-link"
-#######################################################################################
-# Update in pyproject.toml, as uv build backend does not yet support dynamic metadata #
-# https://github.com/astral-sh/uv/issues/11718                                        #
-#######################################################################################
+__app_name__ = "pfmsoft-eve-link"
 __description__ = "A command line first interface to the Eve Online API"
-__version__ = "0.4.0"
+__version__ = version(__app_name__)
 __release__ = __version__
-#######################################################################################
-__url__ = "https://github.com/DonalChilde/eve-esi-link"
+__url__ = "https://github.com/DonalChilde/pfmsoft-eve-link"
 __license__ = "MIT"
 
 
